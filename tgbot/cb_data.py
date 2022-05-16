2a700fdfeaf2e60c85c485672aaf9e451ed8f748
Ninnjah <yand.man4@gmail.com>
2022-05-16 06:35:59 +0300

    Add show ticket callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -1,6 +1,7 @@
 from aiogram.utils.callback_data import CallbackData
 
 cat_select_cb = CallbackData("cat_select", "category")
+show_ticket_cb = CallbackData("show_ticket", "category", "ticket")
 exam_start_cb = CallbackData("exam_start", "category", "ticket")
 menu_explorer_cb = CallbackData("menu_explorer", "menu")
 ticket_cancel_cb = CallbackData("ticket_cancel")
