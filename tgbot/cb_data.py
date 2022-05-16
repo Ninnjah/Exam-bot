6ad92f0d4cd4e72b2a5a5c4b85e7ecc4bbc14b43
Ninnjah <yand.man4@gmail.com>
2022-05-16 06:24:17 +0300

    Remove unnecessary ticket choose callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -3,4 +3,3 @@ from aiogram.utils.callback_data import CallbackData
 cat_select_cb = CallbackData("cat_select", "category")
 exam_start_cb = CallbackData("exam_start", "category", "ticket")
 menu_explorer_cb = CallbackData("menu_explorer", "menu")
-ticket_choose_cb = CallbackData("ticket", "category", "number")
