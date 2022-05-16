1213700f88462bce9ccdc2d1c0868a1289647a55
Ninnjah <yand.man4@gmail.com>
2022-05-16 06:32:51 +0300

    Add ticket cancel callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -3,3 +3,4 @@ from aiogram.utils.callback_data import CallbackData
 cat_select_cb = CallbackData("cat_select", "category")
 exam_start_cb = CallbackData("exam_start", "category", "ticket")
 menu_explorer_cb = CallbackData("menu_explorer", "menu")
+ticket_cancel_cb = CallbackData("ticket_cancel")
