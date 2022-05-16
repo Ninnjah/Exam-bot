45ed9c8842dad3b949494aaa129075ba2e7ba52f
Ninnjah <yand.man4@gmail.com>
2022-05-16 06:06:05 +0300

    Add ticket choose callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -2,3 +2,4 @@ from aiogram.utils.callback_data import CallbackData
 
 exam_start_cb = CallbackData("start_exam", "type")
 menu_explorer_cb = CallbackData("menu_explorer", "menu")
+ticket_choose_cb = CallbackData("ticket", "category", "number")
