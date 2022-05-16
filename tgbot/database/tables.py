9637af629347ea7d0aecec0d4bd65ab9f1f1e50d
Ninnjah <yand.man4@gmail.com>
2022-05-16 04:24:39 +0300

    Add database tables

---
+++ tgbot/database/tables.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, String, BigInteger, Column, DateTime
+
+metadata = MetaData()
+
+users = Table(
+    "users", metadata,
+    Column("user_id", BigInteger(), primary_key=True),
+    Column("firstname", String(), nullable=False),
+    Column("lastname", String(), nullable=True),
+    Column("username", String(), nullable=True),
+    Column("created_on", DateTime(), default=datetime.now),
+    Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now)
+)
+
+pages = Table(
+    "pages", metadata,
+    Column("name", String(), primary_key=True),
+    Column("link", String(), nullable=False),
+    Column("created_on", DateTime(), default=datetime.now),
+    Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now)
+)
