18305edce62a2ef7a9368f2800dfff921dda3b26
Ninnjah <yand.man4@gmail.com>
2022-05-16 05:46:49 +0300

    Add list users handler to admin

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -2,16 +2,36 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
+from tgbot.middlewares.locale import i18n as t
 from tgbot.models.role import UserRole
 from tgbot.services.repository import Repo
 
 
-async def admin_start(m: Message):
-    await m.reply("Hello, admin!")
+async def list_users(m: Message, repo: Repo):
+    users = await repo.list_users()
+
+    if users:
+        msg_text = t("Список пользователей:\n")
+        for num, user in enumerate(users, start=1):
+            fullname = f"{user.firstname}{' ' + user.lastname if user.lastname is not None else ''}"
+
+            msg_text += t(
+                "{num}. <code>{user_id}</code> <b>{fullname}</b> [{date}]\n"
+            ).format(
+                num=num,
+                user_id=user.user_id,
+                fullname=fullname,
+                date=user.created_on.strftime("%Y.%m.%d %H:%M")
+            )
+
+        await m.answer(msg_text)
+
+    else:
+        await m.answer(t("Еще никто не пользовался ботом"))
 
 
 def register_admin(dp: Dispatcher):
-    dp.register_message_handler(admin_start, commands=["start"], state="*", role=UserRole.ADMIN)
+    dp.register_message_handler(list_users, commands=["users"], state="*", role=UserRole.ADMIN)
     # # or you can pass multiple roles:
     # dp.register_message_handler(admin_start, commands=["start"], state="*", role=[UserRole.ADMIN])
     # # or use another filter:
