c54ae276cefd8714ab30a374d9028e946fb12daa
Ninnjah <yand.man4@gmail.com>
2022-05-28 14:26:46 +0300

    Oops. Change donate_link.value in get_kb for success to donate_link

--- tgbot/handlers/exam.py
+++ tgbot/handlers/exam.py
@@ -333,7 +333,7 @@ async def exam_result(callback: CallbackQuery, repo: Repo, state: FSMContext):
                 max_score=max_score,
                 correctness=(current_score / max_score) * 100
             ),
-                reply_markup=exam_end_kb.get_kb(donate_link.value)
+                reply_markup=exam_end_kb.get_kb(donate_link)
             )
 
         except MessageNotModified:
@@ -346,7 +346,7 @@ async def exam_result(callback: CallbackQuery, repo: Repo, state: FSMContext):
                 max_score=max_score,
                 correctness=(current_score / max_score) * 100
             ),
-                reply_markup=exam_end_kb.get_kb(donate_link.value)
+                reply_markup=exam_end_kb.get_kb(donate_link)
             )
 
     else:
