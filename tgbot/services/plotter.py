a7a36ed59ab870d3c0c7966175a9489584de8237
Ninnjah <yand.man4@gmail.com>
2022-06-02 15:58:23 +0300

    Add user top plotter

--- tgbot/services/plotter.py
+++ tgbot/services/plotter.py
@@ -42,3 +42,42 @@ def tickets_plot(data: list):
     plot_file.seek(0)
 
     return plot_file
+
+
+def user_top_plot(data: list):
+    data: DataFrame = DataFrame.from_records(data)
+    all_tickets = data.value_counts("user_id").to_dict()
+
+    # Count of success and failed tickets
+    user_data: dict = data.groupby("user_id")["success"].value_counts().to_dict()
+    success_data: dict = {k[0]: i for k, i in user_data.items() if k[1] == True}
+    failed_data: dict = {k[0]: i for k, i in user_data.items() if k[1] == False}
+
+    # Create plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+    x = [str(x) for x in (all_tickets.keys())]
+    y = list(all_tickets.values())
+    ax.bar(
+        x, y, label="Все билеты",
+        width=0.15, color="b"
+    )
+    x = [str(x) for x in (failed_data.keys())]
+    y = list(failed_data.values())
+    ax.bar(
+        x, y, label="Проваленые билеты",
+        width=0.15, color="r"
+    )
+    x = [str(x) for x in (success_data.keys())]
+    y = list(success_data.values())
+    ax.bar(
+        x, y, label="Успешные билеты",
+        width=0.15, color="g"
+    )
+    fig.legend()
+
+    # Save plot to bytesio
+    plot_file: BytesIO = BytesIO()
+    fig.savefig(plot_file)
+    plot_file.seek(0)
+
+    return plot_file
